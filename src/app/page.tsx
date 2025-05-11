e8507e4dbe15f4689f08d944f7207fc6688f7f1e
Serhii Kushnir <s1983kushnir@gmail.com>
2025-05-11 16:10:23 +0200

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
